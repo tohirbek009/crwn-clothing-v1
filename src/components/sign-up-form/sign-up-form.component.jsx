0d47b810240a8bf9696db60b2f80dc756915f444
tohirbek009 <odilovtohirbek009@gmail.com>
2023-07-30 19:18:20 +0500

    adding cart context and shopping icon as well as cart dropdown

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -21,8 +21,6 @@ const SignUpFrom = () => {
     const [formFields, setFormFields] = useState(defaultFormFields);
     const { displayName, email, password, confirmPassword } = formFields;
 
-    console.log(formFields);
-
     const resetFormFields = () => {
         setFormFields(defaultFormFields);
     }
